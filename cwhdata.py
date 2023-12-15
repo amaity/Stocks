6e1378bd46d3d8d26ba80f0664e5a069f6cf2ae9
amaity <amaity@rediffmail.com>
2023-12-15 07:36:02 +0530

    added a cwh script

---
+++ cwhdata.py
@@ -0,0 +1,5 @@
+#df = pd.DataFrame.from_records([dict(a=1, b=2), dict(a=2, b=3)])
+import pandas as pd
+
+df = pd.DataFrame.from_records([dict(Name='ABBOTINDIA', Target=30849.85, Margin=33, CMP=22742), 
+                                ])
